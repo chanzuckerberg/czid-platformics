5f4530c782c3b4ebeb0bddb6e63142035e56782a
Jessica Gadling <jessica@mootech.net>
2023-07-24 10:02:34 -0700

    [feat] support seeding data for test environments. (#4)

---
+++ entities/test_infra/factories.py
@@ -0,0 +1,62 @@
+import factory
+from factory import Faker, fuzzy
+from faker_biology.bioseq import Bioseq
+from faker_biology.physiology import Organ
+
+from database.models import Sample, SequencingRead
+
+Faker.add_provider(Bioseq)
+Faker.add_provider(Organ)
+
+# TODO, this is a lame singleton to prevent this library from
+# requiring an active SA session at import-time. We should try
+# to refactor it out when we know more about factoryboy
+class SessionStorage:
+    session = None
+
+    @classmethod
+    def set_session(cls, session):
+        cls.session = session
+
+    @classmethod
+    def get_session(cls):
+        return cls.session
+
+
+class CommonFactory(factory.alchemy.SQLAlchemyModelFactory):
+    class Meta:
+        sqlalchemy_session_factory = SessionStorage.get_session
+        sqlalchemy_session_persistence = "commit"
+        sqlalchemy_session = None  # workaround for a bug in factoryboy
+
+class SampleFactory(CommonFactory):
+    class Meta:
+        sqlalchemy_session = None  # workaround for a bug in factoryboy
+        model = Sample
+        # What fields do we try to match to existing db rows to determine whether we
+        # should create a new row or not?
+        sqlalchemy_get_or_create = ('name', 'location')
+
+        # The field in the exclusion list are for internal use only and don't get
+        # persisted to the db.
+        exclude = ('sample_organ',)
+
+    # This is in the exclusion list so it doesn't get added to our db model.
+    sample_organ = factory.Faker('organ')
+
+    name = factory.LazyAttribute(lambda o: 'Sample %s' % o.sample_organ)
+    location = factory.Faker('city')
+
+
+class SequencingReadFactory(CommonFactory):
+    class Meta:
+        sqlalchemy_session = None  # workaround for a bug in factoryboy
+        model = SequencingRead
+        sqlalchemy_get_or_create = ('nucleotide', 'sequence', 'protocol',)
+
+    sample = factory.SubFactory(SampleFactory)
+    nucleotide = fuzzy.FuzzyChoice(['RNA', 'DNA'])
+    # Workaround for a bug in bioseq's handling of randomness and DNA string generation.
+    sequence = fuzzy.FuzzyText(length=100, chars="ACTG")
+    # sequence = factory.Faker('dna', length=100)
+    protocol = fuzzy.FuzzyChoice(['TARGETED', 'MNGS', 'MSSPE'])
