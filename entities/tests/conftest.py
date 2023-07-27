2d350c2832d8c4f00ca3190326d44fd32e147f69
Jessica Gadling <jgadling@chanzuckerberg.com>
2023-07-27 16:40:38 -0700

    Don't install postgres in our Docker image.

--- entities/tests/conftest.py
+++ entities/tests/conftest.py
@@ -1,12 +1,17 @@
 import os
+
 import pytest
+from database.connect import get_db_uri
+from database.models.base import Base
 from pytest_postgresql import factories
 from pytest_postgresql.janitor import DatabaseJanitor
 from sqlalchemy.engine import create_engine
 from sqlalchemy.orm import sessionmaker
-from database.models.base import Base
 
-test_db = factories.postgresql_noproc(host=os.getenv("DB_HOST"), password=os.getenv("DB_PASS"))
+test_db = factories.postgresql_noproc(
+    host=os.getenv("DB_HOST"), password=os.getenv("DB_PASS")
+)
+
 
 @pytest.fixture(scope="session")
 def postgresql(test_db):
@@ -16,7 +21,13 @@ def postgresql(test_db):
     pg_password = test_db.password
     pg_db = test_db.dbname
 
-    with DatabaseJanitor(pg_user, pg_host, pg_port, pg_db, test_db.version, pg_password):
-        engine = create_engine(f"postgresql+psycopg2://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{pg_db}")
+    with DatabaseJanitor(
+        pg_user, pg_host, pg_port, pg_db, test_db.version, pg_password
+    ):
+        engine = create_engine(
+            get_db_uri(
+                db_host=pg_host, db_port=pg_port, db_user=pg_user, db_pass=pg_password
+            )
+        )
         Base.metadata.create_all(engine)
         yield sessionmaker(bind=engine, expire_on_commit=False)
