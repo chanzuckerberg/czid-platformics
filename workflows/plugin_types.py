59ea326c59b12c721c68fa587225bb18edb00964
morsecodist <todd@morsecodist.io>
2023-08-28 08:38:06 -0700

    [CZID-8217] more generic plugin handling and configuration

--- workflows/plugin_types.py
+++ workflows/plugin_types.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Callable, TypedDict, Literal, Optional, Coroutine, Any
+from typing import Callable, List, TypedDict, Literal, Optional, Coroutine, Any
 
 class WorkflowStatusMessage(TypedDict):
     runner_id: str
@@ -8,6 +8,16 @@ class WorkflowStatusMessage(TypedDict):
     error: Optional[str]
 
 class WorkflowRunner(ABC):
+    @abstractmethod
+    def supported_workflow_types(self) -> List[str]:
+        """Returns the supported workflow types, ie ["WDL"]"""
+        raise NotImplementedError()
+
+    @abstractmethod
+    def description(self) -> str:
+        """Returns a description of the workflow runner"""
+        raise NotImplementedError()
+
     @abstractmethod
     async def run_workflow(self, on_complete: Callable[[WorkflowStatusMessage], Coroutine[Any, Any, Any]], workflow_run_id: str, workflow_path: str, inputs: dict) -> str:
         raise NotImplementedError()
