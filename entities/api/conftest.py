9518ae69bf0dec5d886e7b052bd861c81a32f8a6
Jessica Gadling <jgadling@chanzuckerberg.com>
2023-08-29 10:24:11 -0700

    Rearrange test layout.

---
+++ entities/api/conftest.py
@@ -0,0 +1,52 @@
+import json
+import typing
+
+import pytest_asyncio
+from api.core.deps import get_db_session, require_auth_principal, get_auth_principal, get_engine
+from api.main import get_app
+from database.connect import AsyncDB
+from fastapi import FastAPI
+from httpx import AsyncClient
+from sqlalchemy.ext.asyncio import AsyncSession
+from cerbos.sdk.model import Principal
+from starlette.requests import Request
+
+
+async def patched_authprincipal(request: Request) -> Principal:
+    user_id = request.headers.get("user_id")
+    if not user_id:
+        raise Exception("user_id not found in request headers")
+    principal = Principal(
+        user_id,
+        roles=["user"],
+        attr={
+            "user_id": int(user_id),
+            "member_projects": json.loads(request.headers.get("member_projects", "[]")),
+            "admin_projects": json.loads(request.headers.get("admin_projects", "[]")),
+        },
+    )
+    return principal
+
+
+@pytest_asyncio.fixture()
+async def api(
+    async_db: AsyncDB,
+) -> FastAPI:
+    async def patched_session() -> typing.AsyncGenerator[AsyncSession, None]:
+        session = async_db.session()
+        try:
+            yield session
+        finally:
+            await session.close()
+
+    api = get_app()
+    api.dependency_overrides[get_engine] = lambda: async_db
+    api.dependency_overrides[get_db_session] = patched_session
+    api.dependency_overrides[require_auth_principal] = patched_authprincipal
+    api.dependency_overrides[get_auth_principal] = patched_authprincipal
+    return api
+
+
+@pytest_asyncio.fixture()
+async def http_client(api: FastAPI) -> AsyncClient:
+    return AsyncClient(app=api, base_url="http://test")
