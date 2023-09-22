a69c0e347002290f00e05a178404d320bd25521c
Ryan Lim <rlim@chanzuckerberg.com>
2023-09-22 14:44:19 -0700

    remove generate token from entities

--- entities/cli/gqlcli.py
+++ entities/cli/gqlcli.py
@@ -77,32 +77,5 @@ def auth() -> None:
     pass
 
 
-# TODO - this is just a temporary command to help us test the auth flow.
-@auth.command("generate-token")
-@click.argument("userid", required=True, type=int)
-@click.option("--expiration", help="Duration of token in seconds", type=int)
-@click.option(
-    "--project",
-    help="project_id:role associations to include in the header",
-    type=str,
-    default=["123:admin", "123:member", "456:member"],
-    multiple=True,
-)
-@click.pass_context
-def generate_token(ctx: click.Context, userid: int, project: list[str], expiration: int) -> None:
-    settings = Settings.parse_obj({})
-    private_key = settings.JWK_PRIVATE_KEY
-
-    project_dict: dict[int, list[str]] = {}
-    for item in project:
-        project_id, role = item.split(":")
-        if int(project_id) not in project_dict:
-            project_dict[int(project_id)] = []
-        project_dict[int(project_id)].append(role)
-    project_schema = [ProjectRole(project_id=k, roles=v) for k, v in project_dict.items()]
-    token = create_token(private_key, userid, project_schema, expiration)
-    print(token)
-
-
 if __name__ == "__main__":
     cli()
