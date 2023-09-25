5a4fc10126a2c4a7d40b311db779d34440e7b91a
Julie Han <jhan@chanzuckerberg.com>
2023-09-25 10:36:27 -0700

    fix symlinks and update env vars

--- workflows/.happy/terraform/envs/dev/main.tf
+++ workflows/.happy/terraform/envs/dev/main.tf
@@ -9,6 +9,16 @@ module "stack" {
   stack_prefix     = "/${var.stack_name}"
   app_name         = var.app
   deployment_stage = "dev"
+  additional_env_vars = {
+    CERBOS_URL = "http://cerbos.cerbos-system.svc.cluster.local:3592"
+    JWK_PUBLIC_KEY_FILE = "/workflows/test_infra/fixtures/public_key.pem"
+    JWK_PRIVATE_KEY_FILE = "/workflows/test_infra/fixtures/private_key.pem"
+    DEFAULT_UPLOAD_BUCKET = "local-bucket"
+    BOTO_ENDPOINT_URL = "http://motoserver.czidnet:4000"
+    AWS_REGION = "us-west-2"
+    ENTITY_SERVICE_URL = "http://entity-service:8080"
+    ENTITY_SERVICE_AUTH_TOKEN = "test"
+  }
   services = {
     workflows = {
       health_check_path     = "/graphql"
