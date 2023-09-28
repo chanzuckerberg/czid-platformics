2aa66ab0d8c3fdd79ea67d0aa451d3595d274cdb
Ryan Lim <rlim@chanzuckerberg.com>
2023-09-27 17:35:03 -0700

    replace model exports

--- workflows/database/models/__init__.py
+++ workflows/database/models/__init__.py
@@ -1,1 +1,11 @@
-from platformics.database.models.base import Base, meta  # noqa: F401
+from platformics.database.models.base import Base, meta
+from database.models.workflow import (
+    Workflow,
+    WorkflowVersion,
+    RunStatus,
+    Run,
+    RunStep,
+    RunEntityInput,
+)
+
+__all__ = ["Base", "meta", "Workflow", "WorkflowVersion", "RunStatus", "Run", "RunStep", "RunEntityInput"]
