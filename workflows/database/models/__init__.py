8b567659cbb45f27b932be27927d19f6fd6d591d
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2023-09-28 10:14:56 -0700

    Fix import error (#72)
    * Fix import error
    
    * Fix it without noqa

--- workflows/database/models/__init__.py
+++ workflows/database/models/__init__.py
@@ -1,1 +1,4 @@
-from platformics.database.models.base import Base, meta  # noqa: F401
+from platformics.database.models.base import Base, meta
+from database.models.workflow import Workflow, WorkflowVersion, Run, RunStatus, RunStep, RunEntityInput
+
+__all__ = ["Base", "meta", "Workflow", "WorkflowVersion", "Run", "RunStatus", "RunStep", "RunEntityInput"]
