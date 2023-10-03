18bfb9ff89fb0463a74de7257a092070065d8663
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2023-10-03 12:02:07 -0700

    Fix lint errors (#80)

--- entities/scripts/create_db.py
+++ entities/scripts/create_db.py
@@ -1,16 +1,18 @@
 from platformics.api.core.settings import APISettings
 from sqlalchemy_utils import create_database, database_exists
 
-def create_db():
+
+def create_db() -> None:
     settings = APISettings.parse_obj({})
     db_uri = settings.SYNC_DB_URI
     if database_exists(db_uri):
         print("Database already exists!")
-        
+
     else:
         print("Database does not exist, creating database")
         create_database(db_uri)
         print("Database created")
+
 
 if __name__ == "__main__":
     create_db()
