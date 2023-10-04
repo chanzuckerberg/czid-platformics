0238a85e3a2076206837af2450e12694380c5927
rzlim08 <37033997+rzlim08@users.noreply.github.com>
2023-10-04 08:54:50 -0700

    add manifests in seed.py (#76)

--- workflows/scripts/seed.py
+++ workflows/scripts/seed.py
@@ -2,13 +2,18 @@ import factory.random
 from factoryboy import workflow_factory as wf
 from platformics.api.core.settings import APISettings
 from platformics.database.connect import init_sync_db
+from manifest import import_manifests
 
 
-def use_factoryboy():
-    settings = APISettings.parse_obj({})
+def use_factoryboy() -> None:
+    settings = APISettings.model_validate({})
     app_db = init_sync_db(settings.SYNC_DB_URI)
     session = app_db.session()
     wf.SessionStorage.set_session(session)
+
+    # import manifests
+    import_manifests(session=session)
+
     factory.random.reseed_random(1234567)
 
     wf.WorkflowFactory.create_batch(4)
