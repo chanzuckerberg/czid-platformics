29ade9d0b56773b1fded6cf31a7ad839ca38cee2
Ryan Lim <rlim@chanzuckerberg.com>
2023-10-06 15:00:49 -0700

    roll back moto version

--- bin/init_moto.sh
+++ bin/init_moto.sh
@@ -6,7 +6,7 @@
 moto_server --host 0.0.0.0 --port $MOTO_PORT & 
 
 # Initialize data once server is ready
-sleep 1 && curl -X POST "http://motoserver.czidnet:${MOTO_PORT}/moto-api/recorder/replay-recording"
+sleep 3 && curl -X POST "http://motoserver.czidnet:${MOTO_PORT}/moto-api/recorder/replay-recording"
 
 # Go back to moto server
 wait
