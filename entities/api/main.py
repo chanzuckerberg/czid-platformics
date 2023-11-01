bb35d4fdba1a53e9429ada9c849e0560c9ba4b6e
Jessica Gadling <jgadling@chanzuckerberg.com>
2023-11-01 12:44:57 -0400

    Working on dataloaders v2 <interim commit>

--- entities/api/main.py
+++ entities/api/main.py
@@ -68,6 +68,6 @@ def get_app() -> FastAPI:
 app = get_app()
 
 if __name__ == "__main__":
-    config = uvicorn.Config("api.main:app", host="0.0.0.0", port=8008, log_level="info")
+    config = uvicorn.Config("api.main:app", host="0.0.0.0", port=8009, log_level="info")
     server = uvicorn.Server(config)
     server.run()
