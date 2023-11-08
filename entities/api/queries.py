b60dda8f21a45806a43cb232d8a0663081763a24
Julie Han <jhan@chanzuckerberg.com>
2023-11-08 14:02:07 -0800

    readd relay

--- entities/api/queries.py
+++ entities/api/queries.py
@@ -1,6 +1,6 @@
 import typing
 import strawberry
-
+from strawberry import relay
 from api.types.sample import Sample, resolve_sample
 from api.types.sequencing_read import SequencingRead, resolve_sequencing_read
 from api.types.contig import Contig, resolve_contig
@@ -9,6 +9,9 @@ from api.files import File, resolve_files
 
 @strawberry.type
 class Query:
+    # Allow queries by node ID
+    node: relay.Node = relay.node()
+
     samples: typing.Sequence[Sample] = resolve_sample
     sequencing_reads: typing.Sequence[SequencingRead] = resolve_sequencing_read
     contigs: typing.Sequence[Contig] = resolve_contig
