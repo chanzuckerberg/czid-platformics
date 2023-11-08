e0905bb14e11273cc5dda3baa6697e71ef11e83d
Julie Han <jhan@chanzuckerberg.com>
2023-11-08 13:27:09 -0800

    merge conflict

--- entities/api/queries.py
+++ entities/api/queries.py
@@ -1,32 +1,15 @@
 import typing
 import strawberry
-<<<<<<< HEAD
-from strawberry import relay
-from api.types.samples import Sample, resolve_samples
-from api.types.sequencing_reads import SequencingRead, resolve_sequencing_reads
-from api.types.contigs import Contig, resolve_contigs
-=======
 
 from api.types.sample import Sample, resolve_sample
 from api.types.sequencing_read import SequencingRead, resolve_sequencing_read
 from api.types.contig import Contig, resolve_contig
->>>>>>> ca0304c (update templates)
 from api.files import File, resolve_files
 
 
 @strawberry.type
 class Query:
-<<<<<<< HEAD
-    # Allow queries by node ID
-    node: relay.Node = relay.node()
-
-    # Queries for each entity
-    samples: typing.Sequence[Sample] = resolve_samples
-    sequencing_reads: typing.Sequence[SequencingRead] = resolve_sequencing_reads
-    contigs: typing.Sequence[Contig] = resolve_contigs
-=======
     samples: typing.Sequence[Sample] = resolve_sample
     sequencing_reads: typing.Sequence[SequencingRead] = resolve_sequencing_read
     contigs: typing.Sequence[Contig] = resolve_contig
->>>>>>> ca0304c (update templates)
     files: typing.Sequence[File] = resolve_files
