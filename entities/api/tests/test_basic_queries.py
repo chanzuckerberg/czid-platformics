6ad731dbe13c3a541c5cd3aa9bcab356f84241bb
Julie Han <jhan@chanzuckerberg.com>
2023-11-16 13:09:29 -0800

    fix tests

--- entities/api/tests/test_basic_queries.py
+++ entities/api/tests/test_basic_queries.py
@@ -4,7 +4,8 @@ GraphQL tests
 
 import pytest
 from platformics.database.connect import SyncDB
-from test_infra import factories as fa
+from test_infra.factories.main import SessionStorage
+from test_infra.factories.sample import SampleFactory
 from api.conftest import GQLTestClient
 
 
@@ -20,14 +21,14 @@ async def test_graphql_query(
 
     # Create mock data
     with sync_db.session() as session:
-        fa.SessionStorage.set_session(session)
-        fa.SampleFactory.create_batch(
+        SessionStorage.set_session(session)
+        SampleFactory.create_batch(
             2, collection_location="San Francisco, CA", owner_user_id=user_id, collection_id=project_id
         )
-        fa.SampleFactory.create_batch(
+        SampleFactory.create_batch(
             6, collection_location="Mountain View, CA", owner_user_id=user_id, collection_id=project_id
         )
-        fa.SampleFactory.create_batch(
+        SampleFactory.create_batch(
             4, collection_location="Phoenix, AZ", owner_user_id=secondary_user_id, collection_id=9999
         )
 
