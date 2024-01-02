4dadfa56b059774c85b451bab4d74326b26f9461
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2024-01-02 15:54:41 -0800

    [CZID-9034] Refactor and simplify format handlers (#150)
    * Cleanup format handler logic
    
    * Add JSON format validator
    
    * Checkpoint
    
    * Clean up FASTQ validator and create BED validator
    
    * Vastly simplify file handler logic
    
    * Add tests
    
    * Fix CI errors

--- entities/support/format_handlers.py
+++ entities/support/format_handlers.py
@@ -2,11 +2,9 @@
 Logic to validate that a file of a certain format is valid
 """
 
-import gzip
-import tempfile
-import typing
+import io
+import json
 from abc import abstractmethod
-from mypy_boto3_s3.client import S3Client
 from Bio import SeqIO
 from typing import Protocol
 
@@ -18,55 +16,86 @@ class FileFormatHandler(Protocol):
 
     @classmethod
     @abstractmethod
-    def validate(cls, client: S3Client, bucket: str, file_path: str) -> None:
+    def validate(cls, contents: str) -> None:
         raise NotImplementedError
-
-
-class FastqHandler(FileFormatHandler):
-    """
-    Validate FASTQ files (contain sequencing reads)
-    """
-
-    @classmethod
-    def validate(cls, client: S3Client, bucket: str, file_path: str) -> None:
-        fp = get_file_preview(client, bucket, file_path)
-        assert len([read for read in SeqIO.parse(fp, "fastq")]) > 0
 
 
 class FastaHandler(FileFormatHandler):
     """
-    Validate FASTA files (contain sequences)
+    Validate FASTA files. Note that even truncated FASTA files are supported:
+    ">" is a valid FASTA file, and so is ">abc" (without a sequence).
     """
 
     @classmethod
-    def validate(cls, client: S3Client, bucket: str, file_path: str) -> None:
-        fp = get_file_preview(client, bucket, file_path)
-        assert len([read for read in SeqIO.parse(fp, "fasta")]) > 0
+    def validate(cls, contents: str) -> None:
+        sequences = 0
+        for _ in SeqIO.parse(io.StringIO(contents), "fasta"):
+            sequences += 1
+        assert sequences > 0
 
 
-def get_file_preview(client: S3Client, bucket: str, file_path: str) -> typing.TextIO:
+class FastqHandler(FileFormatHandler):
     """
-    Get first 1MB of a file and save it in a temporary file
+    Validate FASTQ files. Can't use biopython directly because large file would be truncated.
+    This removes truncated FASTQ records by assuming 1 read = 4 lines.
     """
-    data = client.get_object(Bucket=bucket, Key=file_path, Range="bytes=0-1000000")["Body"].read()
-    fp = tempfile.NamedTemporaryFile("w+b")
-    fp.write(data)
-    fp.flush()
 
-    try:
-        data.decode("utf-8")
-        return open(fp.name, "r")
-    except UnicodeDecodeError:
-        return gzip.open(fp.name, "rt")
+    @classmethod
+    def validate(cls, contents: str) -> None:
+        # Load file and only keep non-truncated FASTQ records (4 lines per record)
+        fastq = contents.split("\n")
+        fastq = fastq[: len(fastq) - (len(fastq) % 4)]
+
+        # Validate it with SeqIO
+        reads = 0
+        for _ in SeqIO.parse(io.StringIO("\n".join(fastq)), "fastq"):
+            reads += 1
+        assert reads > 0
 
 
-def get_validator(format: str, compression_type: str) -> type[FileFormatHandler]:
+class BedHandler(FileFormatHandler):
+    """
+    Validate BED files using basic checks.
+    """
+
+    @classmethod
+    def validate(cls, contents: str) -> None:
+        # Ignore last line since it could be truncated
+        records = contents.split("\n")[:-1]
+        assert len(records) > 0
+
+        # BED files must have at least 3 columns - error out if the file incorrectly uses spaces instead of tabs
+        num_cols = -1
+        for record in records:
+            assert len(record.split("\t")) >= 3
+            # All rows should have the same number of columns
+            if num_cols == -1:
+                num_cols = len(record.split("\t"))
+            else:
+                assert num_cols == len(record.split("\t"))
+
+
+class JsonHandler(FileFormatHandler):
+    """
+    Validate JSON files
+    """
+
+    @classmethod
+    def validate(cls, contents: str) -> None:
+        json.loads(contents)  # throws an exception for invalid JSON
+
+
+def get_validator(format: str) -> type[FileFormatHandler]:
     """
     Returns the validator for a given file format
     """
-    if format == "fastq":
-        return FastqHandler
-    elif format == "fasta":
+    if format == "fasta":
         return FastaHandler
+    elif format == "fastq":
+        return FastqHandler
+    elif format == "bed":
+        return BedHandler
+    elif format == "json":
+        return JsonHandler
     else:
         raise Exception(f"Unknown file format '{format}'")
---
+++ entities/tests/test_format_handlers.py
@@ -0,0 +1,54 @@
+"""
+Validate that format handlers work as expected
+"""
+
+import pytest
+from support.format_handlers import get_validator
+
+CASES_VALID_FILES = {
+    "fasta": [
+        ">seq1\nACGT\n",
+        ">seq2\n",
+    ],
+    "fastq": [
+        "@read1\nACGT\n+\nAAAA\n",
+    ],
+    "bed": [
+        "chr1\t123\t456\n",
+    ],
+    "json": [
+        '{"a": 1, "b": 2}',
+    ],
+}
+
+CASES_INVALID_FILES = {
+    "fasta": [
+        "@seq1\nACGT\n",
+    ],
+    "fastq": [
+        "@read1\nACGT\n+\nAAA\n",  # sequence field should be same length as quality field
+        "@read1\nACGT\n",  # truncated
+    ],
+    "bed": [
+        "chr1\t123\n",  # need at least 3 columns
+        "chr1\t123\t456\tABC\nchr2\t111\t222\n",  # all rows should have the same number of columns
+    ],
+    "json": [
+        '{"a": 1, "b": }',  # bad JSON (not testing it much because using json.loads() as validator)
+    ],
+}
+
+
+@pytest.mark.parametrize("format", ["fasta", "fastq", "bed", "json"])
+def test_validation_valid_files(format: str) -> None:
+    validator = get_validator(format)
+    for test_case in CASES_VALID_FILES[format]:
+        validator.validate(test_case)
+
+
+@pytest.mark.parametrize("format", ["fasta", "fastq", "bed", "json"])
+def test_validation_invalid_files(format: str) -> None:
+    validator = get_validator(format)
+    for test_case in CASES_INVALID_FILES[format]:
+        with pytest.raises(Exception):
+            validator.validate(test_case)
