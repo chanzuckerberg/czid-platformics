308de039fc603040f903681729a0b1dc7d6a45e0
Jessica Gadling <jgadling@chanzuckerberg.com>
2024-01-19 16:22:26 -0500

    Handle uncaught exceptions (#160)
    * Support masking unexpected errors.

---
+++ platformics/codegen/tests/test_error_handling.py
@@ -0,0 +1,56 @@
+"""
+Test basic error handling
+"""
+
+import pytest
+from platformics.codegen.conftest import GQLTestClient, SessionStorage
+from platformics.codegen.tests.output.test_infra.factories.sample import \
+    SampleFactory
+from platformics.database.connect import SyncDB
+
+
+@pytest.mark.asyncio
+async def test_unauthorized_error(
+    gql_client: GQLTestClient,
+) -> None:
+    """
+    Validate that expected errors don't get masked by our error handler.
+    """
+    query = """
+        mutation createOneSample {
+            createSample(input: {
+                name: "Test Sample"
+                sampleType: "Type 1"
+                waterControl: false
+                collectionLocation: "San Francisco, CA"
+                collectionId: 123
+            }) {
+                id,
+                collectionLocation
+            }
+        }
+    """
+    output = await gql_client.query(query, member_projects=[456])
+
+    # Make sure we haven't masked expected errors.
+    assert output["data"] is None
+    assert "Unauthorized: Cannot create entity in this collection" in output["errors"][0]["message"]
+
+
+@pytest.mark.asyncio
+async def test_python_error(
+    gql_client: GQLTestClient,
+) -> None:
+    """
+    Validate that unexpected errors do get masked by our error handler.
+    """
+    query = """
+        query causeException {
+            uncaughtException
+        }
+    """
+    output = await gql_client.query(query, member_projects=[456])
+
+    # Make sure we have masked unexpected errors.
+    assert output["data"] is None
+    assert "Unexpected error" in output["errors"][0]["message"]
