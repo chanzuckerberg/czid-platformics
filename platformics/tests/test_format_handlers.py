aa82eef75e3ad564847728daf0e5e37c1b785adf
Todd Morse <todd@morsecodist.io>
2024-02-20 21:52:52 -0800

    tests

--- platformics/tests/test_format_handlers.py
+++ platformics/tests/test_format_handlers.py
@@ -2,13 +2,9 @@
 Validate that format handlers work as expected
 """
 
-import os
-from typing import Generator
 import pytest
 from platformics.support.format_handlers import get_validator
 
-import boto3
-from botocore.client import Config
 from mypy_boto3_s3 import S3Client
 
 CASES_VALID_FILES = {
@@ -44,40 +40,20 @@ CASES_INVALID_FILES = {
     ],
 }
 
-@pytest.fixture
-def s3_info() -> Generator[tuple[S3Client, str], None, None]:
-    s3 = boto3.client(
-        "s3",
-        region_name="us-east-1",
-        endpoint_url=os.getenv("BOTO_ENDPOINT_URL"),
-        config=Config(s3={'addressing_style': 'path'}),
-    )
-    bucket = os.environ["DEFAULT_UPLOAD_BUCKET"]
-    try:
-        s3.create_bucket(Bucket=bucket)
-    except Exception:
-        pass
-    for name, values in CASES_VALID_FILES.items():
-        for i, value in enumerate(values):
-            s3.put_object(Bucket=bucket, Key=f"valid-{i}.{name}", Body=value)
-    for name, values in CASES_INVALID_FILES.items():
-        for i, value in enumerate(values):
-            s3.put_object(Bucket=bucket, Key=f"invalid-{i}.{name}", Body=value)
-    yield s3, bucket
-
+BUCKET = "local-bucket"
 
 @pytest.mark.parametrize("format", ["fasta", "fastq", "bed", "json"])
-def test_validation_valid_files(format: str, s3_info: tuple[S3Client, str]) -> None:
-    s3, bucket = s3_info
-    for i, _ in enumerate(CASES_VALID_FILES[format]):
-        validator = get_validator(format)(s3, bucket, f"valid-{i}.{format}")
+def test_validation_valid_files(format: str, moto_client: S3Client) -> None:
+    for i, value in enumerate(CASES_VALID_FILES[format]):
+        moto_client.put_object(Bucket=BUCKET, Key=f"valid-{i}.{format}", Body=value)
+        validator = get_validator(format)(moto_client, BUCKET, f"valid.{format}")
         validator.validate()
 
 
 @pytest.mark.parametrize("format", ["fasta", "fastq", "bed", "json"])
-def test_validation_invalid_files(format: str, s3_info: tuple[S3Client, str]) -> None:
-    s3, bucket = s3_info
-    for i, _ in enumerate(CASES_INVALID_FILES[format]):
-        validator = get_validator(format)(s3, bucket, f"invalid-{i}.{format}")
+def test_validation_invalid_files(format: str, moto_client: S3Client) -> None:
+    for i, value in enumerate(CASES_INVALID_FILES[format]):
+        moto_client.put_object(Bucket=BUCKET, Key=f"invalid-{i}.{format}", Body=value)
+        validator = get_validator(format)(moto_client, BUCKET, f"invalid-{i}.{format}")
         with pytest.raises(Exception):
             validator.validate()
