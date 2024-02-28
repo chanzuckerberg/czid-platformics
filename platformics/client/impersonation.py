6a65aaa1571a95c62b9989d5bdd6213422c6733e
Ryan Lim <rlim@chanzuckerberg.com>
2024-02-28 10:34:38 -0800

    add token regen after 1 hour

--- platformics/client/impersonation.py
+++ platformics/client/impersonation.py
@@ -2,6 +2,7 @@ import os
 import logging
 from platformics.settings import Settings
 from platformics.security.token_auth import create_token
+import time
 
 import httpx
 
@@ -9,16 +10,25 @@ IDENTITY_SERVICE_URL = os.environ["IDENTITY_SERVICE_BASE_URL"]
 
 class ImpersonationClient:
     def __init__(self) -> None:
+        self.time_created = None
+        self._token = self.get_token()
+    
+    def get_token(self)->str:
         settings = Settings.model_validate({})
         private_key = settings.JWK_PRIVATE_KEY
-        self._token = create_token(private_key, None, None, 3600, "workflows")
+        if self.time_created is None or (time.time() - self.time_created) > 3600:
+            self.time_created = time.time()
+            self._token = create_token(private_key, None, None, 3600, "workflows")
+            return self._token
+        else:
+            return self._token
 
     async def impersonate(self, user_id: int) -> str:
         async with httpx.AsyncClient(follow_redirects=True) as client:
             logger = logging.getLogger()
             resp = await client.get(
                 f"{IDENTITY_SERVICE_URL}/impersonate/?user_id={user_id}",
-                headers={"Authorization": f"Bearer {self._token}"},
+                headers={"Authorization": f"Bearer {self.get_token()}"},
             )
             try:
                 return resp.json()["token"]
