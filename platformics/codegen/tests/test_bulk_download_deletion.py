00dd1a870e39e540617cde47193af0191ce57c8b
Julie Han <jhan@chanzuckerberg.com>
2024-03-26 16:24:54 -0700

    add test for deleting old bulk downlaods

---
+++ platformics/codegen/tests/test_bulk_download_deletion.py
@@ -0,0 +1,59 @@
+"""
+Test deletion of bulkDownloads > 7 days old
+"""
+
+import pytest
+import datetime
+from platformics.database.connect import SyncDB
+from platformics.codegen.conftest import SessionStorage, GQLTestClient, FileFactory
+from platformics.codegen.tests.output.test_infra.factories.bulk_download import BulkDownloadFactory
+
+@pytest.mark.asyncio
+async def test_delete_old_bulk_downloads(
+    sync_db: SyncDB,
+    gql_client: GQLTestClient,
+) -> None:
+    """
+    Test that we can make cascade deletions
+    """
+    user_id = 12345
+    project_id = 123
+
+    # Create mock data: 2 SequencingReads, each with a different Sample, and each with R1/R2
+    with sync_db.session() as session:
+        SessionStorage.set_session(session)
+        current_time = datetime.datetime.now()
+        one_week_ago = current_time - datetime.timedelta(days=7)
+        one_month_ago = current_time - datetime.timedelta(days=30)
+
+        current_bulk_downloads = BulkDownloadFactory.create_batch(3, owner_user_id=user_id, collection_id=project_id)
+        one_week_old_bulk_downloads = BulkDownloadFactory.create_batch(2, owner_user_id=user_id, collection_id=project_id, created_at=one_week_ago)
+        one_month_old_bulk_downloads = BulkDownloadFactory.create_batch(5, owner_user_id=user_id, collection_id=project_id, created_at=one_month_ago)
+        all_old_bulk_downloads = one_week_old_bulk_downloads + one_month_old_bulk_downloads
+        FileFactory.update_file_ids()
+
+    # Delete old bulk downloads
+    query = """
+        mutation MyMutation {
+            deleteOldBulkDownloads {
+                id
+            }
+        }
+    """
+
+    result = await gql_client.query(query, user_id=user_id, member_projects=[project_id], service_identity="rails")
+    assert len(result["data"]["deleteOldBulkDownloads"]) == 7
+    assert [bd["id"] for bd in result["data"]["deleteOldBulkDownloads"]] == [str(bd.id) for bd in all_old_bulk_downloads]
+
+    # Check that current bulk downloads are still there
+    query = """
+        query MyQuery {
+            bulkDownloads {
+                id
+            }
+        }
+    """
+
+    result = await gql_client.query(query, user_id=user_id, member_projects=[project_id])
+    assert len(result["data"]["bulkDownloads"]) == 3
+    assert [bd["id"] for bd in result["data"]["bulkDownloads"]] == [str(bd.id) for bd in current_bulk_downloads]
