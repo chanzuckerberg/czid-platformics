b795ca27c0429a23f511eaadcfbd4cb8c7c63f3e
Todd Morse <todd@morsecodist.io>
2024-03-27 19:15:17 -0400

    Restructure event bus to allow them to control message commit behavior (#273)

--- workflows/api/loader/loader.py
+++ workflows/api/loader/loader.py
@@ -5,6 +5,7 @@ Loader functions
 import asyncio
 import json
 import sys
+from typing import Awaitable, Callable
 from platformics.client.impersonation import ImpersonationClient
 from platformics.util.types_utils import JSONValue
 
@@ -14,7 +15,13 @@ from sqlalchemy.orm import joinedload, selectinload
 from sqlalchemy.ext.asyncio import AsyncSession
 from api.config import resolve_output_loader
 
-from plugins.plugin_types import EventBus, WorkflowFailedMessage, WorkflowStartedMessage, WorkflowSucceededMessage
+from plugins.plugin_types import (
+    EventBus,
+    WorkflowFailedMessage,
+    WorkflowStartedMessage,
+    WorkflowStatusMessage,
+    WorkflowSucceededMessage,
+)
 from database.models import WorkflowVersion, WorkflowRun
 from manifest.manifest import EntityInput, Manifest
 from support.enums import WorkflowRunStatus
@@ -67,62 +74,62 @@ class LoaderDriver:
             )
         await asyncio.gather(*loader_futures)
 
+    def _bind_handle_message(self) -> Callable[[WorkflowStatusMessage], Awaitable[None]]:
+        async def handle_message(event: WorkflowStatusMessage) -> None:
+            print("event", event, file=sys.stderr)
+            if isinstance(event, WorkflowStartedMessage):
+                workflow_run = (
+                    await self.session.execute(select(WorkflowRun).where(WorkflowRun.execution_id == event.runner_id))
+                ).scalar_one_or_none()
+                if workflow_run and workflow_run.status in [
+                    WorkflowRunStatus.CREATED,
+                    WorkflowRunStatus.PENDING,
+                    WorkflowRunStatus.STARTED,
+                ]:
+                    workflow_run.status = WorkflowRunStatus.RUNNING
+                    await self.session.commit()
+
+            if isinstance(event, WorkflowSucceededMessage):
+                _event: WorkflowSucceededMessage = event
+                result = await self.session.execute(
+                    select(WorkflowRun)
+                    .options(
+                        joinedload(WorkflowRun.workflow_version).joinedload(WorkflowVersion.workflow),
+                        selectinload(WorkflowRun.entity_inputs),
+                    )
+                    .where(WorkflowRun.execution_id == _event.runner_id)
+                )
+                workflow_run = result.scalar_one_or_none()
+                if workflow_run:
+                    workflow_version = workflow_run.workflow_version
+                    entity_inputs = Manifest.normalize_inputs(
+                        (
+                            entity_input.field_name,
+                            EntityInput(
+                                entity_type=entity_input.type,
+                                entity_id=str(entity_input.input_entity_id),
+                            ),
+                        )
+                        for entity_input in workflow_run.entity_inputs
+                    )
+                    await self.process_workflow_completed(workflow_version, workflow_run, entity_inputs, _event.outputs)
+                    workflow_run.status = WorkflowRunStatus.SUCCEEDED
+                    await self.session.commit()
+            if isinstance(event, WorkflowFailedMessage):
+                workflow_run = (
+                    await self.session.execute(select(WorkflowRun).where(WorkflowRun.execution_id == event.runner_id))
+                ).scalar_one_or_none()
+                if workflow_run:
+                    workflow_run.status = WorkflowRunStatus.FAILED
+                    if event.error:
+                        workflow_run.error_message = event.error
+                    await self.session.commit()
+
+        return handle_message
+
     async def main(self) -> None:
         """Waits for events and if a workflow completes, runs the loaders"""
         print("Running listener")
         while True:
-            for event in await self.bus.poll():
-                print("event", event, file=sys.stderr)
-                if isinstance(event, WorkflowStartedMessage):
-                    workflow_run = (
-                        await self.session.execute(
-                            select(WorkflowRun).where(WorkflowRun.execution_id == event.runner_id)
-                        )
-                    ).scalar_one_or_none()
-                    if workflow_run and workflow_run.status in [
-                        WorkflowRunStatus.CREATED,
-                        WorkflowRunStatus.PENDING,
-                        WorkflowRunStatus.STARTED,
-                    ]:
-                        workflow_run.status = WorkflowRunStatus.RUNNING
-                        await self.session.commit()
-
-                if isinstance(event, WorkflowSucceededMessage):
-                    _event: WorkflowSucceededMessage = event
-                    result = await self.session.execute(
-                        select(WorkflowRun)
-                        .options(
-                            joinedload(WorkflowRun.workflow_version).joinedload(WorkflowVersion.workflow),
-                            selectinload(WorkflowRun.entity_inputs),
-                        )
-                        .where(WorkflowRun.execution_id == _event.runner_id)
-                    )
-                    workflow_run = result.scalar_one_or_none()
-                    if workflow_run:
-                        workflow_version = workflow_run.workflow_version
-                        entity_inputs = Manifest.normalize_inputs(
-                            (
-                                entity_input.field_name,
-                                EntityInput(
-                                    entity_type=entity_input.type,
-                                    entity_id=str(entity_input.input_entity_id),
-                                ),
-                            )
-                            for entity_input in workflow_run.entity_inputs
-                        )
-                        await self.process_workflow_completed(
-                            workflow_version, workflow_run, entity_inputs, _event.outputs
-                        )
-                        workflow_run.status = WorkflowRunStatus.SUCCEEDED
-                        await self.session.commit()
-
-                if isinstance(event, WorkflowFailedMessage):
-                    workflow_run = (
-                        await self.session.execute(
-                            select(WorkflowRun).where(WorkflowRun.execution_id == event.runner_id)
-                        )
-                    ).scalar_one_or_none()
-                    if workflow_run:
-                        workflow_run.status = WorkflowRunStatus.FAILED
-                        await self.session.commit()
+            await self.bus.poll(self._bind_handle_message())
             await asyncio.sleep(1)
