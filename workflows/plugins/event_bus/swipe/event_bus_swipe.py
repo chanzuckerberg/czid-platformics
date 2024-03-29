8830dd97c584ce014f67937726bcc2da7583e6d5
Ryan Lim <rlim@chanzuckerberg.com>
2024-03-29 16:09:10 -0700

    fix status messsage

--- workflows/plugins/event_bus/swipe/event_bus_swipe.py
+++ workflows/plugins/event_bus/swipe/event_bus_swipe.py
@@ -67,10 +67,7 @@ class EventBusSWIPE(EventBus):
         # TODO: handle aws.batch for step statuses
         if not message.get("source") == "aws.states":
             return None
-        if not message.get("status"):
-            print("No status in message: ", message)
-            return None
-        status = self._create_workflow_status(message["status"])
+        status = self._create_workflow_status(message["detail"]["status"])
         execution_arn = message["detail"]["executionArn"]
         if status == "WORKFLOW_SUCCESS":
             return WorkflowSucceededMessage(
