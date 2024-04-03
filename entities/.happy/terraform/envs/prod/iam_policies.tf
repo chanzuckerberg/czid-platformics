d441fdb6ff7703b9a4f5237a0993a2a088ccf869
rzlim08 <37033997+rzlim08@users.noreply.github.com>
2024-04-03 11:39:19 -0700

    prod deploy workflows entities (#256)
    * add permissions to s3 bucket entities
    
    * initial prod deploy workflows and entities
    
    * remove development s3 bucket from prod permissions
    
    * actually remove dev policies
    
    * bump cpu and memory

---
+++ entities/.happy/terraform/envs/prod/iam_policies.tf
@@ -0,0 +1,31 @@
+data "aws_iam_policy_document" "entities" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "secretsmanager:GetSecretValue",
+    ]
+    resources = ["arn:aws:secretsmanager:us-west-2:${var.aws_account_id}:secret:*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "kms:Decrypt",
+    ]
+    resources = ["*"]
+  }
+    statement {
+    effect = "Allow"
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:ListBucketMultipartUploads",
+      "s3:ListBucket",
+      "s3:PutObjectTagging"
+    ]
+    resources = [
+      "arn:aws:s3:::idseq-prod-samples-us-west-2",
+      "arn:aws:s3:::idseq-prod-samples-us-west-2/*"
+    ]
+  }
+}
