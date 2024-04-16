e9a0d60c2c9629f088efe009f9e2fb75b8b69d84
rzlim08 <37033997+rzlim08@users.noreply.github.com>
2024-04-16 10:35:00 -0700

    create dev enviroment fixes (#292)
    * create dev enviroment fixes
    
    * comment out block
    
    * add output stackname
    
    * try with workflows
    :
    
    * remove sqs check
    
    * revert comment
    
    * update queue name
    
    * shorten service name
    
    * re-add entities
    
    * add service name to workflows dev

--- workflows/.happy/terraform/envs/dev/main.tf
+++ workflows/.happy/terraform/envs/dev/main.tf
@@ -81,12 +81,13 @@ module "stack" {
     PLATFORMICS_EVENT_BUS_PLUGIN                          = "swipe"
     PLATFORMICS_EVENT_BUS__REDIS__QUEUE_NAME              = "workflow-events"
     PLATFORMICS_EVENT_BUS__REDIS__REDIS_URL               = "redis://redis.czidnet:6378"
-    PLATFORMICS_EVENT_BUS__SWIPE__SQS_QUEUE_URL           = "https://sqs.us-west-2.amazonaws.com/${var.aws_account_id}/idseq-swipe-development-nextgen-web-sfn-notifications-queue"
+    PLATFORMICS_EVENT_BUS__SWIPE__SQS_QUEUE_URL           = "https://sqs.us-west-2.amazonaws.com/${var.aws_account_id}/idseq-swipe-dev-nextgen-web-sfn-notifications-queue"
     PLATFORMICS_WORKFLOW_RUNNER_PLUGIN                    = "swipe"
     PLATFORMICS_WORKFLOW_RUNNER__LOCAL__S3_ENDPOINT       = ""
     PLATFORMICS_WORKFLOW_RUNNER__SWIPE__OUTPUT_S3_PREFIX  = "s3://idseq-samples-development/nextgen/"
     PLATFORMICS_WORKFLOW_RUNNER__SWIPE__STATE_MACHINE_ARN = "arn:aws:states:us-west-2:${var.aws_account_id}:stateMachine:idseq-swipe-development-default-wdl"
     WORKERS                                               = "2"
+    SERVICE_NAME                                          = "workflows"
   }
   create_dashboard = false
   routing_method = "CONTEXT"
